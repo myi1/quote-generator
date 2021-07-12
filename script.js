410da0580d57ac3689cbc3f3543e834533ef6a92
Muhammad Ismail <yahyaismail@gmail.com>
2021-07-12 01:47:30 -0400

    Script.js: Add newQuote and tweetQuote functionality

--- script.js
+++ script.js
@@ -1,1 +1,68 @@
-console.log("hello, world!");
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+let apiQuotes = [];
+
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
+// Show New Quote
+function newQuote() {
+  loading();
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  //  Check if Author field is blank and replace it with 'Unknown'
+  if (!quote.author) {
+    authorText.innerText = "Unknown";
+  } else {
+    authorText.innerText = quote.author;
+  }
+  //   Check Quote Length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  //   Set Quote, Hide Loader
+  quoteText.innerText = quote.text;
+  complete();
+}
+
+// Get Quotes from API
+async function getQuotes() {
+  loading();
+  const apiURL = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiURL);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    console.log(error);
+    // Catch Error Here
+  }
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// On Load
+getQuotes();
